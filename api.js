7e05ea879f64049e13ab79597c4aa1cd32da01ee
maxi-gartner <gartner.maxi@hotmail.com>
2024-07-26 23:49:28 -0300

    desplegando

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 
-let apiUrl = 'http://localhost:8000/'
+//let apiUrl = 'http://localhost:8000/'
+let apiUrl = import.meta.env.VITE_API_URL;
 
  if (process.env.NODE_ENV === "production") {
     apiUrl = import.meta.env.VITE_API
