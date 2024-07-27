6564ef0a4c801a23f5dd088eae66bec867cfd2b7
maxi-gartner <gartner.maxi@hotmail.com>
2024-07-27 00:15:45 -0300

    27/7

--- api.js
+++ api.js
@@ -1,6 +1,7 @@
 
 //let apiUrl = 'http://localhost:8000/'
-let apiUrl = import.meta.env.VITE_API_URL;
+//let apiUrl = import.meta.env.VITE_API_URL;
+let apiUrl = "https://homeessentials-back-dev-propia.onrender.com"
 
  if (process.env.NODE_ENV === "production") {
     apiUrl = import.meta.env.VITE_API
